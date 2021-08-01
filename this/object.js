11d134ac1e1121c9a25c04d3da0511e583d59494
Jack Do <jackie.duc.do@gmail.com>
2021-08-01 12:13:55 +0700

    add browser

--- this/object.js
+++ this/object.js
@@ -5,6 +5,10 @@
 const bob = {
   name: "Bob Marley",
   age: 25,
+  sleep() {
+    //writing like this or using function like below is exactly the same
+    console.log("sleeping...");
+  },
   greeting: function () {
     console.log("Hi there, would like some music!");
   },
@@ -13,6 +17,8 @@ const bob = {
   },
 };
 
+console.log(bob);
+bob.sleep();
 bob.introduce(); // Hello, my name is Bob Marley
 
 /**
@@ -20,3 +26,18 @@ bob.introduce(); // Hello, my name is Bob Marley
  * once we use constructor function to create object, then the value
  * changes according to the object that was created
  */
+
+/** create object using constructor function */
+function Animal(type) {
+  this.type = type;
+}
+
+/** create object using class */
+
+class Bird {
+  constructor(type) {
+    this.type = type;
+  }
+}
+
+/** either way, 'this' will always point to the object that they created */
