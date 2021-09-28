99a305a506937b722d4760379eb5cb04e9f0cc4f
Jack Do <jackie.duc.do@gmail.com>
2021-09-28 21:39:54 +0700

    interesting to initialize object

---
+++ OOP/init-object-another-way.js
@@ -0,0 +1,67 @@
+/**
+ * Init first way
+ */
+
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  sayMyname() {
+    console.log(`My name is ${this.name}`);
+  }
+}
+
+const peter = new Person('Peter', 14);
+peter.sayMyname();
+
+/**
+ * Init second way
+ */
+
+class Teacher {
+  constructor(methods) {
+    Object.keys(methods).forEach((m) => (this[m] = methods[m]));
+    this.type = 'teacher';
+  }
+  update(callback) {
+    callback(this);
+  }
+}
+
+const jessie = new Teacher({
+  setup(...args) {
+    args.forEach((subject) => this.subjects.push(subject));
+    this.name = 'Jessie';
+    this.update((teacher) => {
+      teacher.favoriteColor = 'Blue';
+      teacher.shopping = function () {
+        console.log('Going shopping');
+      };
+    });
+  },
+});
+
+const ben = new Teacher({
+  setup(...args) {
+    args.forEach((subject) => this.subjects.push(subject));
+    this.name = 'Ben';
+    this.age = 35;
+    this.update((teacher) => {
+      teacher.hobby = 'Fishing';
+      teacher.exercise = (type) => {
+        console.log(`Working out ${type}`);
+      };
+    });
+  },
+});
+
+const teachers = [jessie, ben];
+teachers.forEach((t) => {
+  t.subjects = [];
+  t.setup('Physics', 'Maths');
+});
+
+console.log(jessie);
+console.log(ben);
